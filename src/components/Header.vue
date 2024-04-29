016042c90fc4f7c66af324008aeab118c0bbc064
Mark Atherton <mark@insightinternet.co.uk>
2024-04-29 15:14:43 +0100

    added same 2-col grid layout to Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1 className="text-3xl font-bold text-red-500 underline text-center">
-    Insight Internet
-  </h1>
+  <div className="grid grid-cols-2 gap-4">
+    <div>
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">
+        Insight Internet
+      </h1>
+    </div>
+    <div>Sumit here!</div>
+  </div>
 </template>
