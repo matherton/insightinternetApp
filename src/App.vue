572ac651816e9a6fa55376207133ebb4988cddea
Mark Atherton <mark@insightinternet.co.uk>
2024-04-29 14:59:07 +0100

    added 2-col grid layout in App

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,18 @@ import "./style.css";
 <template>
   <Header />
   <br />
-  <div style="min-height: 90%">body content</div>
+  <div className="grid grid-cols-2 gap-4">
+    <div>01</div>
+    <div>02</div>
+    <div>03</div>
+    <div>04</div>
+    <div>01</div>
+    <div>02</div>
+    <div>03</div>
+    <div>04</div>
+  </div>
   <br />
   <Footer />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
