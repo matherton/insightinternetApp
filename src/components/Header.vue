7f6e0d0eab674aecca59a2e2db66e860f45a0f45
Mark Atherton <mark@insightinternet.co.uk>
2024-05-16 21:21:58 +0100

    added responsive footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -53,30 +53,10 @@
       </div>
     </div>
   </nav>
-
-  <div className="grid grid-cols-2">
-    <div>
-      <img
-        class="logo"
-        src="/src/assets/II-logo-clean.png"
-        alt="Insight Internet: Invent, Implement, React"
-      />
-    </div>
-    <div>
-      <ul class="flex">
-        <li class="ml-6">
-          <a class="text-blue-500 hover:text-blue-800" href="#">Home</a>
-        </li>
-        <li class="ml-6">
-          <a class="text-blue-500 hover:text-blue-800" href="#">Services</a>
-        </li>
-        <li class="ml-6">
-          <a class="text-blue-500 hover:text-blue-800" href="#">About Us</a>
-        </li>
-        <li class="ml-6">
-          <a class="text-blue-500 hover:text-blue-800" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
 </template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
