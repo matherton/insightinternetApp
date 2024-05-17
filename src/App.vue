8028b9eaaace16d6a3c8670c8c8168f0c647e3bc
Mark Atherton <mark@insightinternet.co.uk>
2024-05-17 15:05:32 +0100

    added Page component and was able to pass data into it but not into the class

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Page from "./components/Page.vue";
 import Footer from "./components/Footer.vue";
 import "./style.css";
+
+import { defineProps } from "vue";
+
+defineProps({
+  myName: {
+    type: String,
+  },
+  gridColLG: {
+    type: Number,
+    default: 2,
+  },
+});
 </script>
 
 <template>
   <Header />
   <br />
-  <div class="grid grid-cols-2 gap-4">
-    <div>01</div>
-    <div>02</div>
-    <div>03</div>
-    <div>04</div>
-    <div>01</div>
-    <div>02</div>
-    <div>03</div>
-    <div>04</div>
-  </div>
+  <Page myName="Mark" :gridColLG="2" />
   <br />
   <Footer />
 </template>
 
 <style scoped></style>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    Page,
+  },
+};
+</script>
---
+++ src/components/Page.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  myName: String,
+  gridColLG: Number,
+});
+</script>
+<!-- TODO: gridColLG prop display corectly in tags but not correctly in the class??? -->
+<template lang="">
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-{{gridColLG}} gap-4"
+  >
+    <div>{{ myName }}</div>
+    <div>{{ gridColLG }}</div>
+    <div>03</div>
+    <div>04</div>
+    <div>01</div>
+    <div>02</div>
+    <div>03</div>
+    <div>04</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Page",
+};
+</script>
+<style lang=""></style>
