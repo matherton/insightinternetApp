e4a6e4b191706ba3c4dea7e146b9678214d5c088
Mark Atherton <mark@insightinternet.co.uk>
2024-05-23 15:42:42 +0100

    changed contact link to a button and styled the same as Get Started button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,42 +17,22 @@ defineProps({
           <sub class="slogan">Invent, Implement, React</sub>
         </div>
       </div>
-      <!-- 
-      <div class="block lg:hidden">
-        <button
-          class="flex items-center px-3 py-2 border rounded button-primary border-teal-400 hover:text-white hover:border-white"
-        >
-          <svg
-            class="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div> -->
       <div
         class="w-full block flex-grow pt-4 lg:flex lg:items-center lg:w-auto"
       >
         <div class="text-sm lg:flex-grow">
-          <a href="#responsive-header" class="button-primary px-4 py-2 mr-2"
-            >Home</a
-          >
-          <a href="#responsive-header" class="button-primary px-4 py-2 mr-2"
-            >Services</a
-          >
-          <a href="#responsive-header" class="button-primary px-4 py-2 mr-2"
-            >About</a
-          >
+          <a href="/" class="button-primary px-4 py-2 mr-2">Home</a>
+          <a href="/services" class="button-primary px-4 py-2 mr-2">Services</a>
+          <a href="/about" class="button-primary px-4 py-2 mr-2">About</a>
         </div>
         <div>
-          <a
+          <button
             href="#"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            class="button-secondary px-4 py-2"
             @click="showModal"
-            >Contact</a
           >
+            Contact
+          </button>
         </div>
       </div>
     </nav>
