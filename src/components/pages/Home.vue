cd26429ea9caa32ac07301e19a93983e5c867c6b
Mark Atherton <mark@insightinternet.co.uk>
2024-05-23 11:47:39 +0100

    removed margin top on central home image nd replaced with MeM.png

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -7,7 +7,7 @@ import Page from "../Page.vue";
     title1="Empowering Startups with Cutting-Edge React Applications"
     subTitle1="Transform your business with custom-built React solutions tailored to your needs."
     copy1="At InsightInternet, we specialize in delivering high-quality React applications to help startups succeed. Our team of experienced developers is dedicated to providing innovative solutions that drive growth and efficiency."
-    imgName="II-logo-clean"
+    imgName="MeM"
     title2="About InsightInternet"
     subTitle2="Story"
     copy2="InsightInternet was founded with a vision to support startups by providing top-notch React applications. Based in Edinburgh and partnering with Edinburgh Startups and small businesses, we leverage the latest technologies to create seamless, responsive, and scalable applications."
