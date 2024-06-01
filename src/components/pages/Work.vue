84d57184cb5e70caa3316e6318948254f4d19e7d
Mark Atherton <mark@insightinternet.co.uk>
2024-06-01 12:41:55 +0100

    created a Work page by altering About

---
+++ src/components/pages/Work.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Page from "../Page.vue";
+</script>
+
+<template>
+  <Page
+    title1="Why Choose Us"
+    subTitle1="Expertise:"
+    copy1="Highly skilled React development with a wealth of experience in building dynamic and responsive web applications. We offer bespoke solutions for startups and small businesses tailored to their specific needs without the limitaitions and cost of industry leaders such as Wix and Squarespace."
+    img2ndCol="typing.gif"
+    subTitle2="Collaboration:"
+    copy2="We work closely with our clients to understand their goals and deliver solutions that exceed expectations."
+    subTitle3="Innovation:"
+    copy3="At InsightInternet, we stay at the forefront of technology trends to bring the most advanced solutions to your business."
+    subTitle4="Reliability:"
+    copy4="Depend on us for consistent, high-quality service and ongoing support."
+    subTitle5="Pojects:"
+    copy5="Previous experience with various clients available on my portfolio website."
+  />
+</template>
+
+<script>
+export default {
+  name: "Work",
+};
+</script>
