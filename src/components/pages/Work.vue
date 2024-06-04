a41a9c61aa01a5fcdf2facdd2d7e6d581d98ec10
Mark Atherton <mark@insightinternet.co.uk>
2024-06-04 15:28:47 +0100

    setup cards for previous work

--- src/components/pages/Work.vue
+++ src/components/pages/Work.vue
@@ -1,10 +1,56 @@
 <script setup>
-import Page from "../Page.vue";
 import Card from "../utilities/Card.vue";
+import connectLogo from "/src/assets/connect-auto.png";
 </script>
 
 <template>
-  <Card header="Card Header" body="Card Body" footer="Card Footer" />
+  <div class="wrapper">
+    <p>
+      Explore some of the projects we have successfully completed for our
+      clients. Each project highlights our commitment to excellence and our
+      expertise in delivering top-notch React applications.
+    </p>
+
+    <div class="flex flex-wrap">
+      <div class="w-full md:w-1/4 xl:w-1/4">
+        <!-- Column 1 content -->
+        <Card
+          imgSrc="https://imgs.search.brave.com/3zvhcaTE_a-hTnRRddvadsgioBLKrZ9awcQ2eGdT3QI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/dHlwLnVzL2ZpbGUv/bGxveWRzLnN2Zw.svg"
+          header="Lloyd's Banking Group"
+          body="Provided React consulting services to Lloyds Banking Group through Sapient"
+          footer="Card Footer"
+        />
+      </div>
+      <div class="w-full md:w-1/4 xl:w-1/4">
+        <!-- Column 2 content -->
+        <Card
+          imgSrc="https://upload.wikimedia.org/wikipedia/commons/f/f5/Arnold_Clark_Logo.svg"
+          header="Arnold Clark"
+          body="Provided React Native development services to Arnold Clark developing their mobile car reservation app"
+          footer="Card Footer"
+        />
+      </div>
+      <div class="w-full md:w-1/4 xl:w-1/4">
+        <!-- Column 3 content -->
+        <Card
+          imgSrc="https://imgs.search.brave.com/3zvhcaTE_a-hTnRRddvadsgioBLKrZ9awcQ2eGdT3QI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/dHlwLnVzL2ZpbGUv/bGxveWRzLnN2Zw.svg"
+          header="Lloyd's Banking Group"
+          body="Provided React consulting services to Lloyds Banking Group through BJSS working on their loan's React application"
+          footer="Card Footer"
+        />
+      </div>
+      <div class="w-full md:w-1/4 xl:w-1/4">
+        <!-- Column 4 content -->
+        <Card
+          :imgSrc="connectLogo"
+          header="Connect-Auto"
+          body="Employed React Web Application development services to Connect-Auto"
+          footer="Card Footer"
+          class="imgSpacer"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,3 +58,8 @@ export default {
   name: "Work",
 };
 </script>
+<style scoped>
+.imgSpacer img {
+  margin: 3.5rem 0;
+}
+</style>
--- src/components/utilities/Card.vue
+++ src/components/utilities/Card.vue
@@ -1,12 +1,47 @@
+<script setup></script>
 <template>
-  <t-card header="Title of the card">
-    Content of the card.
-
-    <template v-slot:footer>
-      <div class="flex justify-between">
-        <t-button type="button"> Cancel </t-button>
-        <t-button type="button"> Ok </t-button>
-      </div>
-    </template>
-  </t-card>
+  <v-card>
+    <v-card-image><img :src="imgSrc" /></v-card-image>
+    <v-card-title
+      ><strong>{{ header }}</strong></v-card-title
+    >
+    <v-card-body>{{ body }}</v-card-body>
+    <v-card-footer>{{ footer }}</v-card-footer>
+  </v-card>
 </template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    header: String,
+    body: String,
+    footer: String,
+    imgSrc: String,
+  },
+};
+</script>
+
+<style scoped>
+v-card {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 350px;
+  margin: 1rem;
+  padding: 1rem;
+  border: 0.25rem solid var(--secondary-dark-gray);
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
+  border-bottom: 0.15rem dashed var(--accent-green);
+}
+
+v-card img {
+  max-height: 221px;
+  width: 100%;
+}
+
+.imgSpacer img {
+  margin: 3.5rem 0;
+}
+</style>
