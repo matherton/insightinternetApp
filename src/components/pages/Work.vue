6e979bdee4c6e72bf27d642d0a022f20bde70ccc
Mark Atherton <mark@insightinternet.co.uk>
2024-06-04 12:39:59 +0100

    added a card component and tried to import into the work page

--- src/components/pages/Work.vue
+++ src/components/pages/Work.vue
@@ -1,22 +1,10 @@
 <script setup>
 import Page from "../Page.vue";
+import Card from "../utilities/Card.vue";
 </script>
 
 <template>
-  <Page
-    title1="Previous Work"
-    subTitle1="Expertise:"
-    copy1="Highly skilled React development with a wealth of experience in building dynamic and responsive web applications. We offer bespoke solutions for startups and small businesses tailored to their specific needs without the limitaitions and cost of industry leaders such as Wix and Squarespace."
-    img2ndCol="typing.gif"
-    subTitle2="Collaboration:"
-    copy2="We work closely with our clients to understand their goals and deliver solutions that exceed expectations."
-    subTitle3="Innovation:"
-    copy3="At InsightInternet, we stay at the forefront of technology trends to bring the most advanced solutions to your business."
-    subTitle4="Reliability:"
-    copy4="Depend on us for consistent, high-quality service and ongoing support."
-    subTitle5="Pojects:"
-    copy5="Previous experience with various clients available on my portfolio website."
-  />
+  <Card header="Card Header" body="Card Body" footer="Card Footer" />
 </template>
 
 <script>
---
+++ src/components/utilities/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <t-card header="Title of the card">
+    Content of the card.
+
+    <template v-slot:footer>
+      <div class="flex justify-between">
+        <t-button type="button"> Cancel </t-button>
+        <t-button type="button"> Ok </t-button>
+      </div>
+    </template>
+  </t-card>
+</template>
