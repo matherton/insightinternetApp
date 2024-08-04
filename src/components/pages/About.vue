2dc192cfacd302ab85ccb094c4bb0dc50c3f0bd6
Mark Atherton <mark@insightinternet.co.uk>
2024-08-04 11:35:17 +0100

    corrected spelling of limitations

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -6,7 +6,7 @@ import Page from "../Page.vue";
   <Page
     title1="Why Choose Us"
     subTitle1="Expertise:"
-    copy1="Highly skilled React development with a wealth of experience in building dynamic and responsive web applications. We offer bespoke solutions for startups and small businesses tailored to their specific needs without the limitaitions and cost of industry leaders such as Wix and Squarespace."
+    copy1="Highly skilled React development with a wealth of experience in building dynamic and responsive web applications. We offer bespoke solutions for startups and small businesses tailored to their specific needs without the limitations and cost of industry leaders such as Wix and Squarespace."
     img2ndCol="typing.gif"
     subTitle2="Collaboration:"
     copy2="We work closely with our clients to understand their goals and deliver solutions that exceed expectations."
